c2c02b6b70643627f2636264e11d25ff4f2f3d5d
Yoann Gini <yoann.gini@gmail.com>
2017-03-07 13:00:36 +0100

    update metadata

--- cookbooks/chef_run_once/metadata.rb
+++ cookbooks/chef_run_once/metadata.rb
@@ -1,19 +1,19 @@
 name 'chef_run_once'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Yoann Gini'
+maintainer_email 'yoann.gini@gmail.com'
 license 'all_rights'
-description 'Installs/Configures chef_run_once'
-long_description 'Installs/Configures chef_run_once'
+description 'Installs chef-run-once command line'
+long_description 'The chef-run-once command line allow a end user without admin right to run chef service on demand'
 version '0.1.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/chef_run_once/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/ygini/chef-cookbooks/issues' if respond_to?(:issues_url)
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/chef_run_once' if respond_to?(:source_url)
+source_url 'https://github.com/ygini/chef-cookbooks' if respond_to?(:source_url)
