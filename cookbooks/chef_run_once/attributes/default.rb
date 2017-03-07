c5d28142f03a4033d4bc15b02f3be0557395761b
Yoann Gini <yoann.gini@gmail.com>
2017-03-07 13:48:38 +0100

    Update default path to fit macOS SIP

--- cookbooks/chef_run_once/attributes/default.rb
+++ cookbooks/chef_run_once/attributes/default.rb
@@ -5,6 +5,6 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 default['chef_run_once'] = {}
-default['chef_run_once']['chef_run_once_path'] = '/usr/bin/chef-run-once'
+default['chef_run_once']['chef_run_once_path'] = '/usr/local/bin/chef-run-once'
 default['chef_run_once']['cmd_to_run_chef'] = '/bin/launchctl start com.chef.chef-client'
 default['chef_run_once']['chef_run_once_allowed_sudoer'] = '%staff'
